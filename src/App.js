8e8279ae2a5b6fed6421a46c187cfb0b16c0bab8
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-11-15 13:16:26 +0200

    styling/adding states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+import Circle from "./Circle";
+import React, { Component } from "react";
+import { circles } from "./circles";
 
-function App() {
-  return <div className="App">Hello world</div>;
+class App extends Component {
+  state = {
+    score: 0,
+  };
+
+  clickHandler = () => {
+    this.setState({ score: this.state.score + 10 });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <header>Speed Game</header>
+        <h1>Your score: {this.state.score}</h1>
+        <div className="circles">
+          {circles.map((c) => (
+            <Circle
+              key={c.id}
+              color={c.color}
+              id={c.id}
+              click={this.clickHandler}
+            />
+          ))}
+        </div>
+
+        <div className="buttons">
+          <button>Start</button>
+          <button>Stop</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
