1eaa8e68a4a4de3bc239a4b2713bc014a3d66c33
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-11-16 10:59:57 +0200

    checking the correct click

--- src/App.js
+++ src/App.js
@@ -14,12 +14,19 @@ class App extends Component {
     score: 0,
     current: 0,
     gameOver: false,
+    pace: 1500,
   };
 
   time = undefined;
-  pace = 1500;
 
-  clickHandler = () => {
+  clickHandler = (id) => {
+    console.log("you clicked; ", id);
+
+    if (this.state.current !== id) {
+      this.stopHandler();
+      return;
+    }
+
     this.setState({ score: this.state.score + 10 });
   };
 
@@ -32,10 +39,10 @@ class App extends Component {
 
     this.setState({
       current: nextActive,
+      pace: this.state.pace * 0.95,
     });
 
-    this.pace *= 0.95;
-    this.timer = setTimeout(this.nextCircle, this.pace);
+    this.timer = setTimeout(this.nextCircle, this.state.pace);
 
     console.log("active circle is", this.state.current);
   };
@@ -49,13 +56,24 @@ class App extends Component {
 
     this.setState({
       gameOver: true,
+      current: 0,
+    });
+  };
+
+  closeHandler = () => {
+    this.setState({
+      gameOver: false,
+      score: 0,
+      pace: 1500,
     });
   };
 
   render() {
     return (
       <div className="app">
-        {this.state.gameOver && <GameOver score={this.state.score} />}
+        {this.state.gameOver && (
+          <GameOver score={this.state.score} close={this.closeHandler} />
+        )}
         <header>Speed Game</header>
         <h1>Your score: {this.state.score}</h1>
         <div className="circles">
@@ -64,7 +82,7 @@ class App extends Component {
               key={c.id}
               color={c.color}
               id={c.id}
-              click={this.clickHandler}
+              click={() => this.clickHandler(c.id)}
               active={this.state.current === c.id}
               score={this.state.score}
             />
--- src/GameOver.js
+++ src/GameOver.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const closeHandler = () => {
-  window.location.reload();
-};
+// const closeHandler = () => {
+//   window.location.reload();
+// };
+
 
 const GameOver = (props) => {
   return (
@@ -10,7 +11,7 @@ const GameOver = (props) => {
       <div className="popup">
         <h1>Game Over</h1>
         <p>Your score was: {props.score}</p>
-        <button className="close" onClick={closeHandler}>
+        <button className="close" onClick={props.close}>
           x
         </button>
       </div>
